4eb77c3acdf81aac01f556d0d72a158c65674936
Firoz <fza884@outlook.com>
2025-03-02 14:40:06 +0530

    Resolved merge conflicts

--- Day 8 - Functions in Python.py
+++
@@ -1,7 +0,0 @@
-def greet():
-    print("Hello Firoz")
-    print("How do you do")
-    print("Isn't the weather nice")
-
-greet()
-
--- Day 8 - Functions in Python1.py
+++
@@ -1,7 +0,0 @@
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}")
-    print(f"Isn't the weather nice {name}")
-
-greet_with_name("Firoz")
-greet_with_name("Firoz")
